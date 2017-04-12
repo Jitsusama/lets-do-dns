4e7332c37c8e09a5387ddcb2dcbb058120be95f6
Joel Gerber <joel@grrbrr.ca>
2017-04-12 17:33:05 -0400

    Improve Functional Test Comments
    This is more to help me keep track of the feature requirements than
    anything else. Using the idea of user stories to try and drive the logic
    of my system tests.

--- tests/system/test_acme_dns_authentication.py
+++ tests/system/test_acme_dns_authentication.py
@@ -8,6 +8,8 @@ from acmednsauth.authenticate import Authenticate
 def test_digitalocean_authentication_record_creation(
         capsys, api_key, domain, fqdn, auth_token, temporary_filename,
         authorization_header, base_uri):
+    # Emulate certbot passing in proper environment variables for
+    # the authentication step.
     create_environment = {
         'DO_API_KEY': api_key,
         'DO_DOMAIN': domain,
@@ -15,11 +17,15 @@ def test_digitalocean_authentication_record_creation(
         'CERTBOT_VALIDATION': auth_token,
     }
 
+    # Exercise DNS authentication process.
     Authenticate(environment=create_environment)
 
+    # Verify that the authentication process writes record id to STDOUT.
     record_id, _ = capsys.readouterr()
     assert int(record_id) > 0
 
+    # Verify that the authentication process created the proper DNS record
+    # with DigitalOcean.
     request_uri = '%s/%s/%d' % (base_uri, domain, record_id)
     response = get(request_uri, headers=authorization_header)
     record_data = response.json()['domain_record']
@@ -27,5 +33,5 @@ def test_digitalocean_authentication_record_creation(
         record_data['name'] == fqdn and \
         record_data['data'] == auth_token
 
-    # Cleanup
+    # Manually cleanup the created DigitalOcean authentication DNS record.
     delete(request_uri, headers=authorization_header)
