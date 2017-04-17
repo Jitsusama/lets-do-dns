7be5d164a190ce36450f6e8b3608acf9a1b62b3c
Joel Gerber <joel@grrbrr.ca>
2017-04-17 16:55:36 -0400

    Record.create() Stores Record ID Internally

--- do_record/record.py
+++ do_record/record.py
@@ -7,14 +7,20 @@ class Record(object):
     """Handle DigitalOcean DNS records."""
 
     def __init__(self, api_key, domain, hostname):
-        self.api_key = api_key
+        self._id = None
         self.domain = domain
         self.hostname = hostname
+        self.api_key = api_key
 
     def create(self, value):
         """Create this record on DigitalOcean with the supplied value."""
-        return http.create(self, value)
+        self._id = http.create(self, value)
+        return self.id
 
     def delete(self, record_id):
         """Delete this record on DigitalOcean, identified by record_id."""
         http.delete(self, record_id)
+
+    @property
+    def id(self):
+        return self._id
--- tests/unit/test_record.py
+++ tests/unit/test_record.py
@@ -2,7 +2,18 @@ from do_record import Record
 import pytest
 
 
-@pytest.mark.parametrize('input_record_id', [345678, 456789])
+@pytest.mark.parametrize('input_record_id', [491834, 882342])
+def test_create_stores_record_id_internally(mocker, env, input_record_id):
+    mocker.patch(
+        'do_record.record.http.create', return_value=input_record_id)
+
+    record = Record(env.key, env.domain, env.hostname)
+    record.create(env.auth_token)
+
+    assert record.id == input_record_id
+
+
+@pytest.mark.parametrize('input_record_id', [345678])
 def test_create_returns_record_id_from_created_dns_record(
         mocker, env, input_record_id):
     mocker.patch(
@@ -23,7 +34,7 @@ def test_create_properly_calls_http_create(mocker, env):
     stub_http_create.assert_called_once_with(record, env.auth_token)
 
 
-@pytest.mark.parametrize('record_id', [491834, 882342])
+@pytest.mark.parametrize('record_id', [491834])
 def test_delete_properly_calls_http_delete(mocker, env, record_id):
     stub_http_delete = mocker.patch('do_record.record.http.delete')
 
