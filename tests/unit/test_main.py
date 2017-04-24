7beafffcab2d788d47fdfb9fb9f099cd25421b2b
Joel Gerber <joel@grrbrr.ca>
2017-04-24 16:09:37 -0400

    Put Test Function Into Interaction Focused Classes
    The collection of test functions was growing quite large. For now
    I'm putting these into classes that are focused against the
    interactions main() has with external objects. I might eventually
    break these out into separate test modules.

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -4,110 +4,110 @@ from mock import ANY, call
 import pytest
 
 
-def test_passes_environment_to_authenticate(mocker):
-    mocker.patch('certbot_dns_auth.__main__.sys.exit')
-    mocker.patch('certbot_dns_auth.__main__.Arguments')
-    mocker.patch('certbot_dns_auth.__main__.Environment')
+class TestAuthenticateInteractions(object):
+    def test_passed_environment(self, mocker):
+        mocker.patch('certbot_dns_auth.__main__.sys.exit')
+        mocker.patch('certbot_dns_auth.__main__.Arguments')
+        mocker.patch('certbot_dns_auth.__main__.Environment')
 
-    stub_environment = mocker.patch(
-        'certbot_dns_auth.__main__.os.environ')
-    stub_authenticate = mocker.patch(
-        'certbot_dns_auth.__main__.Authenticate')
+        stub_environment = mocker.patch(
+            'certbot_dns_auth.__main__.os.environ')
+        stub_authenticate = mocker.patch(
+            'certbot_dns_auth.__main__.Authenticate')
 
-    main()
+        main()
 
-    stub_authenticate.assert_called_once_with(stub_environment, ANY)
+        stub_authenticate.assert_called_once_with(stub_environment, ANY)
 
+    def test_passed_arguments(self, mocker):
+        mocker.patch('certbot_dns_auth.__main__.os.environ')
+        mocker.patch('certbot_dns_auth.__main__.sys')
+        mocker.patch('certbot_dns_auth.__main__.Environment')
+        mocker.patch(
+            'certbot_dns_auth.__main__.Arguments', return_value=1)
 
-@pytest.mark.parametrize('return_code', [0, 1])
-def test_exits_with_authenticates_return_code(mocker, return_code):
-    mocker.patch('certbot_dns_auth.__main__.Authenticate.__init__',
-                 return_value=None)
-    mocker.patch('certbot_dns_auth.__main__.Authenticate.perform',
-                 return_value=return_code)
-    mocker.patch('certbot_dns_auth.__main__.Arguments')
-    mocker.patch('certbot_dns_auth.__main__.Environment')
+        stub_authenticate = mocker.patch(
+            'certbot_dns_auth.__main__.Authenticate')
 
-    stub_exit = mocker.patch('certbot_dns_auth.__main__.sys.exit')
+        main()
 
-    main()
+        stub_authenticate.assert_called_once_with(ANY, 1)
 
-    stub_exit.assert_called_once_with(return_code)
+    @pytest.mark.parametrize('return_code', [0, 1])
+    def test_performs_return_code_passed_to_sys_exit(
+            self, mocker, return_code):
+        mocker.patch('certbot_dns_auth.__main__.Authenticate.__init__',
+                     return_value=None)
+        mocker.patch('certbot_dns_auth.__main__.Authenticate.perform',
+                     return_value=return_code)
+        mocker.patch('certbot_dns_auth.__main__.Arguments')
+        mocker.patch('certbot_dns_auth.__main__.Environment')
 
+        stub_exit = mocker.patch('certbot_dns_auth.__main__.sys.exit')
 
-def test_passes_cli_arguments_to_arguments(mocker):
-    arguments = ['lets-do-dns', '--help']
+        main()
 
-    mocker.patch('certbot_dns_auth.__main__.os.environ')
-    mocker.patch('certbot_dns_auth.__main__.Authenticate')
-    mocker.patch('certbot_dns_auth.__main__.Environment')
-    mocker.patch('certbot_dns_auth.__main__.sys', argv=arguments)
-
-    stub_arguments = mocker.patch('certbot_dns_auth.__main__.Arguments')
-
-    main()
-
-    stub_arguments.assert_called_once_with(arguments)
-
-
-def test_passes_arguments_to_authenticate(mocker):
-    mocker.patch('certbot_dns_auth.__main__.os.environ')
-    mocker.patch('certbot_dns_auth.__main__.sys')
-    mocker.patch('certbot_dns_auth.__main__.Environment')
-    mocker.patch(
-        'certbot_dns_auth.__main__.Arguments', return_value=1)
-
-    stub_authenticate = mocker.patch(
-        'certbot_dns_auth.__main__.Authenticate')
-
-    main()
-
-    stub_authenticate.assert_called_once_with(ANY, 1)
+        stub_exit.assert_called_once_with(return_code)
 
 
-def test_passes_os_environ_to_environment(mocker):
-    mocker.patch('certbot_dns_auth.__main__.sys')
-    mocker.patch('certbot_dns_auth.__main__.Authenticate')
-    mocker.patch('certbot_dns_auth.__main__.Arguments')
+class TestArgumentsInteractions(object):
+    def test_passed_cli_arguments(self, mocker):
+        arguments = ['lets-do-dns', '--help']
 
-    stub_environ = mocker.patch('certbot_dns_auth.__main__.os.environ')
-    stub_environment = mocker.patch(
-        'certbot_dns_auth.__main__.Environment')
+        mocker.patch('certbot_dns_auth.__main__.os.environ')
+        mocker.patch('certbot_dns_auth.__main__.Authenticate')
+        mocker.patch('certbot_dns_auth.__main__.Environment')
+        mocker.patch('certbot_dns_auth.__main__.sys', argv=arguments)
 
-    main()
+        stub_arguments = mocker.patch(
+            'certbot_dns_auth.__main__.Arguments')
 
-    stub_environment.assert_called_once_with(stub_environ)
+        main()
 
-
-def test_exits_with_code_two_when_environment_throws_missing_exception(
-        mocker):
-    mocker.patch('certbot_dns_auth.__main__.os.environ')
-    mocker.patch('certbot_dns_auth.__main__.sys.argv')
-    mocker.patch('certbot_dns_auth.__main__.Authenticate')
-    mocker.patch('certbot_dns_auth.__main__.Arguments')
-    mocker.patch('certbot_dns_auth.__main__.stderr')
-    mocker.patch(
-        'certbot_dns_auth.__main__.Environment',
-        side_effect=RequiredInputMissing())
-
-    stub_exit = mocker.patch('certbot_dns_auth.__main__.sys.exit')
-
-    main()
-
-    stub_exit.assert_has_calls([call(2)])
+        stub_arguments.assert_called_once_with(arguments)
 
 
-def test_passes_environment_exception_message_to_printer(mocker):
-    mocker.patch('certbot_dns_auth.__main__.os.environ')
-    mocker.patch('certbot_dns_auth.__main__.sys')
-    mocker.patch('certbot_dns_auth.__main__.Authenticate')
-    mocker.patch('certbot_dns_auth.__main__.Arguments')
-    mocker.patch(
-        'certbot_dns_auth.__main__.Environment',
-        side_effect=RequiredInputMissing('Missing Required Input'))
+class TestEnvironmentInteractions(object):
+    def test_passed_os_environ(self, mocker):
+        mocker.patch('certbot_dns_auth.__main__.sys')
+        mocker.patch('certbot_dns_auth.__main__.Authenticate')
+        mocker.patch('certbot_dns_auth.__main__.Arguments')
 
-    stub_printer = mocker.patch('certbot_dns_auth.__main__.stderr')
+        stub_environ = mocker.patch('certbot_dns_auth.__main__.os.environ')
+        stub_environment = mocker.patch(
+            'certbot_dns_auth.__main__.Environment')
 
-    main()
+        main()
 
-    stub_printer.assert_called_once_with('Missing Required Input')
+        stub_environment.assert_called_once_with(stub_environ)
+
+    def test_raised_error_code_passed_to_sys_exit(self, mocker):
+        mocker.patch('certbot_dns_auth.__main__.os.environ')
+        mocker.patch('certbot_dns_auth.__main__.sys.argv')
+        mocker.patch('certbot_dns_auth.__main__.Authenticate')
+        mocker.patch('certbot_dns_auth.__main__.Arguments')
+        mocker.patch('certbot_dns_auth.__main__.stderr')
+        mocker.patch(
+            'certbot_dns_auth.__main__.Environment',
+            side_effect=RequiredInputMissing())
+
+        stub_exit = mocker.patch('certbot_dns_auth.__main__.sys.exit')
+
+        main()
+
+        stub_exit.assert_has_calls([call(2)])
+
+    def test_raised_error_message_passed_to_stderr(self, mocker):
+        mocker.patch('certbot_dns_auth.__main__.os.environ')
+        mocker.patch('certbot_dns_auth.__main__.sys')
+        mocker.patch('certbot_dns_auth.__main__.Authenticate')
+        mocker.patch('certbot_dns_auth.__main__.Arguments')
+        mocker.patch(
+            'certbot_dns_auth.__main__.Environment',
+            side_effect=RequiredInputMissing('Missing Required Input'))
+
+        stub_printer = mocker.patch('certbot_dns_auth.__main__.stderr')
+
+        main()
+
+        stub_printer.assert_called_once_with('Missing Required Input')
