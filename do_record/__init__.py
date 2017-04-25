0c8cc62bfdb29bc0e55e8cf24a2e42f1d47e8923
Joel Gerber <joel@grrbrr.ca>
2017-04-24 20:27:25 -0400

    Move record from do_record to certbot_dns_auth.do_domain

--- do_record/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Handles DigitalOcean Domain Record CRD."""
-
-from .record import Record
-
-__all__ = (Record)
--- tests/unit/test_record.py
+++ tests/unit/test_record.py
@@ -1,13 +1,15 @@
-from do_record.record import Record
-from mock import call
 import pytest
+from mock import call
+
+from certbot_dns_auth.do_domain.record import Record
 
 
 @pytest.mark.parametrize('input_record_id', [491834, 882342])
 def test_create_stores_record_id_internally(mocker, env, input_record_id):
-    mocker.patch('do_record.record.Resource.create')
     mocker.patch(
-        'do_record.record.Resource.__int__',
+        'certbot_dns_auth.do_domain.record.Resource.create')
+    mocker.patch(
+        'certbot_dns_auth.do_domain.record.Resource.__int__',
         return_value=input_record_id)
 
     record = Record(env.key, env.domain, env.hostname)
@@ -17,7 +19,8 @@ def test_create_stores_record_id_internally(mocker, env, input_record_id):
 
 
 def test_create_properly_calls_http_create(mocker, env):
-    stub_http_create = mocker.patch('do_record.record.Resource')
+    stub_http_create = mocker.patch(
+        'certbot_dns_auth.do_domain.record.Resource')
 
     record = Record(env.key, env.domain, env.hostname)
     record.create(env.auth_token)
@@ -29,7 +32,8 @@ def test_create_properly_calls_http_create(mocker, env):
 
 @pytest.mark.parametrize('record_id', [491834])
 def test_delete_properly_calls_http_delete(mocker, env, record_id):
-    stub_http_delete = mocker.patch('do_record.record.Resource')
+    stub_http_delete = mocker.patch(
+        'certbot_dns_auth.do_domain.record.Resource')
 
     record = Record(env.key, env.domain, env.hostname)
     record.number = record_id
@@ -41,7 +45,8 @@ def test_delete_properly_calls_http_delete(mocker, env, record_id):
 
 
 def test_printer_calls_printer(mocker, env):
-    stub_printer = mocker.patch('do_record.record.stdout')
+    stub_printer = mocker.patch(
+        'certbot_dns_auth.do_domain.record.stdout')
     record_id = 918342
 
     record = Record(env.key, env.domain, env.hostname)
