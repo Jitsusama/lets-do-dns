81731d1680ed3289e8d50f17a6b26d5538b89e2c
Joel Gerber <joel@grrbrr.ca>
2017-04-27 18:39:54 -0400

    Cleanup system test py.test fixtures
    Made things more explicit by not wrapping up data within the env
    fixture. Instead have each item called out explicitly.

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+from requests import post
 
 
 @pytest.fixture(autouse=True)
@@ -11,22 +12,34 @@ def os_environ_reset():
     os.environ.update(original_env)
 
 
+@pytest.fixture
+def do_record_id(create_response):
+    return create_response.json()['domain_record']['id']
+
+
+@pytest.fixture
+def create_response(
+        do_base_uri, do_auth_header, do_domain, do_hostname, request):
+    return post(
+        '%s/%s/records' % (do_base_uri, do_domain),
+        headers=do_auth_header,
+        json={'type': 'TXT',
+              'name': do_hostname,
+              'data': request.function.__name__})
+
+
+@pytest.fixture
+def do_base_uri():
+    return 'https://api.digitalocean.com/v2/domains'
+
+
+@pytest.fixture
+def do_auth_header(do_api_key):
+    return {'Authorization': 'Bearer %s' % do_api_key}
+
+
 @pytest.fixture()
-def env(api_key):
-    class Environment(object):
-        def __init__(self):
-            self.key = api_key
-            self.auth_header = {'Authorization': 'Bearer %s' % self.key}
-            self.auth_token = 'validate-with-thing'
-            self.domain = 'grrbrr.ca'
-            self.hostname = 'test-ssl-host'
-            self.base_uri = 'https://api.digitalocean.com/v2/domains'
-
-    return Environment()
-
-
-@pytest.fixture()
-def api_key():
+def do_api_key():
     file_path = os.path.realpath(__file__)
     directory_path = os.path.dirname(file_path)
     api_key_file = '%s/digital_ocean_api_key.txt' % directory_path
@@ -34,3 +47,13 @@ def api_key():
     api_key_file = open(api_key_file, 'r')
 
     return api_key_file.readline()
+
+
+@pytest.fixture
+def do_domain():
+    return 'grrbrr.ca'
+
+
+@pytest.fixture
+def do_hostname():
+    return 'test-ssl-host'
--- tests/system/test_lets-do-dns_script.py
+++ tests/system/test_lets-do-dns_script.py
@@ -6,81 +6,79 @@ from requests import get, delete, post
 # ATTENTION: Look at conftest.py for py.test fixture definitions.
 
 
-def test_pre_authentication_hook(env):
+def test_pre_authentication_hook(
+        do_base_uri, do_auth_header, do_api_key, do_domain, do_hostname):
     os.environ.update({
-        'DO_APIKEY': env.key,
-        'DO_DOMAIN': env.domain,
-        'CERTBOT_DOMAIN': '%s.%s' % (env.hostname, env.domain),
-        'CERTBOT_VALIDATION': env.auth_token,
+        'DO_APIKEY': do_api_key,
+        'DO_DOMAIN': do_domain,
+        'CERTBOT_DOMAIN':
+            '%s.%s' % (do_hostname, do_domain),
+        'CERTBOT_VALIDATION':
+            'test_pre_authentication_hook',
     })
 
     program_output = subprocess.check_output('lets-do-dns')
     record_id = program_output.decode()
 
-    assert int(record_id) > 0
-
     request_uri = '%s/%s/records/%s' % (
-        env.base_uri, env.domain, record_id)
-    response = get(request_uri, headers=env.auth_header)
-    record_data = response.json()['domain_record']
-    expected_hostname = '_acme-challenge.%s' % env.hostname
+        do_base_uri, do_domain, record_id)
 
-    assert (record_data['type'] == 'TXT' and
-            record_data['name'] == expected_hostname and
-            record_data['data'] == env.auth_token)
+    try:
+        response = get(request_uri, headers=do_auth_header)
+        record_data = response.json()['domain_record']
+        expected_hostname = '_acme-challenge.%s' % do_hostname
 
-    delete(request_uri, headers=env.auth_header)
+        assert (record_data['type'] == 'TXT' and
+                record_data['name'] == expected_hostname and
+                record_data['data'] == 'test_pre_authentication_hook')
+
+    finally:  # we always want to delete the created record.
+        delete(request_uri, headers=do_auth_header)
 
 
-def test_post_authentication_hook_without_post_command(env):
-    create_response = post(
-        '%s/%s/records' % (env.base_uri, env.domain),
-        headers=env.auth_header,
-        json={'type': 'TXT',
-              'name': env.hostname,
-              'data': env.auth_token})
-    record_id = create_response.json()['domain_record']['id']
-
+def test_post_authentication_hook_without_post_command(
+        do_base_uri, do_auth_header, do_api_key, do_domain, do_hostname,
+        do_record_id):
     os.environ.update({
-        'DO_APIKEY': env.key,
-        'DO_DOMAIN': env.domain,
-        'CERTBOT_DOMAIN': '%s.%s' % (env.hostname, env.domain),
-        'CERTBOT_VALIDATION': env.auth_token,
-        'CERTBOT_AUTH_OUTPUT': str(record_id)
+        'DO_APIKEY': do_api_key,
+        'DO_DOMAIN': do_domain,
+        'CERTBOT_DOMAIN':
+            '%s.%s' % (do_hostname, do_domain),
+        'CERTBOT_VALIDATION':
+            'test_post_authentication_hook_without_post_command',
+        'CERTBOT_AUTH_OUTPUT':
+            str(do_record_id)
     })
 
     subprocess.check_call('lets-do-dns')
 
     request_uri = '%s/%s/records/%s' % (
-        env.base_uri, env.domain, record_id)
-    get_response = get(request_uri, headers=env.auth_header)
+        do_base_uri, do_domain, do_record_id)
+    get_response = get(request_uri, headers=do_auth_header)
 
     assert get_response.status_code == 404
 
 
-def test_post_authentication_hook_with_post_command(env):
-    create_response = post(
-        '%s/%s/records' % (env.base_uri, env.domain),
-        headers=env.auth_header,
-        json={'type': 'TXT',
-              'name': env.hostname,
-              'data': env.auth_token})
-    record_id = create_response.json()['domain_record']['id']
-
+def test_post_authentication_hook_with_post_command(
+        do_base_uri, do_auth_header, do_api_key, do_domain, do_hostname,
+        do_record_id):
     os.environ.update({
-        'DO_APIKEY': env.key,
-        'DO_DOMAIN': env.domain,
-        'CERTBOT_DOMAIN': '%s.%s' % (env.hostname, env.domain),
-        'CERTBOT_VALIDATION': env.auth_token,
-        'CERTBOT_AUTH_OUTPUT': str(record_id),
+        'DO_APIKEY': do_api_key,
+        'DO_DOMAIN': do_domain,
         'LETS_DO_POSTCMD': 'echo hello',
+        'CERTBOT_DOMAIN':
+            '%s.%s' % (do_hostname, do_domain),
+        'CERTBOT_VALIDATION':
+            'test_post_authentication_hook_with_post_command',
+        'CERTBOT_AUTH_OUTPUT':
+            str(do_record_id),
     })
 
     postcmd_output = subprocess.check_output('lets-do-dns')
 
     request_uri = '%s/%s/records/%s' % (
-        env.base_uri, env.domain, record_id)
-    get_response = get(request_uri, headers=env.auth_header)
+        do_base_uri, do_domain, do_record_id)
+    get_response = get(request_uri, headers=do_auth_header)
 
     assert (get_response.status_code == 404 and
             postcmd_output.decode() == 'hello\n')
