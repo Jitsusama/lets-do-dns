e884e7577681e6b62e309bd0899bc273652950ef
Joel Gerber <joel@grrbrr.ca>
2017-04-27 19:23:34 -0400

    Change tests to mock out print() instead of stderr/stdout
    Since lets_do_dns.printer now uses print() from __future__, I no
    longer need to try and mock out the sys.stdx objects, which is good,
    since tox don't like that.

--- tests/unit/test_printer.py
+++ tests/unit/test_printer.py
@@ -1,19 +1,20 @@
+import sys
 from lets_do_dns.printer import stdout, stderr
 from mock import call
 import pytest
 
 
 def test_printer_writes_to_stdout(mocker):
-    stub_stdout = mocker.patch('sys.stdout.write')
+    stub_stdout = mocker.patch('lets_do_dns.printer.print')
 
     stdout(234567)
 
-    stub_stdout.assert_has_calls([call('234567')])
+    stub_stdout.assert_has_calls([call(234567)])
 
 
 @pytest.mark.parametrize('message', ['', None])
 def test_printer_writes_nothing_with_empty_message(mocker, message):
-    stub_stdout = mocker.patch('sys.stdout.write')
+    stub_stdout = mocker.patch('lets_do_dns.printer.print')
 
     stdout(message)
 
@@ -21,16 +22,16 @@ def test_printer_writes_nothing_with_empty_message(mocker, message):
 
 
 def test_stderr_writes_to_stderr(mocker):
-    stub_stderr = mocker.patch('sys.stderr.write')
+    stub_stderr = mocker.patch('lets_do_dns.printer.print')
 
     stderr('error message')
 
-    stub_stderr.assert_has_calls([call('error message')])
+    stub_stderr.assert_has_calls([call('error message', file=sys.stderr)])
 
 
 @pytest.mark.parametrize('message', ['', None])
 def test_stderr_writes_nothing_with_empty_message(mocker, message):
-    stub_stderr = mocker.patch('sys.stderr.write')
+    stub_stderr = mocker.patch('lets_do_dns.printer.print')
 
     stderr(message)
 
