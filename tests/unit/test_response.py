ffb8f54357d88d4a8da19889f6330999187cc294
Joel Gerber <joel@grrbrr.ca>
2017-05-01 16:14:29 -0400

    Removed custom py.test fixtures from Response() tests
    Switched to using MagicMock()s instead of custom fixtures in order
    to make the test function more explicit, hopefully easier to
    understand in isolation, and remove logic from my test setup, which
    should hopefully make the tests less fragile.

--- tests/unit/test_response.py
+++ tests/unit/test_response.py
@@ -1,53 +1,71 @@
 import pytest
+import requests
 
 from lets_do_dns.errors import RecordCreationFailure
 from lets_do_dns.do_domain.response import Response
 
-# ATTENTION: Look at conftest.py for py.test fixture definitions.
-
 
 @pytest.mark.parametrize('record_id', [23465545])
-def test_resource_id_returns_integer_on_post_request(
-        fake_requests_post_response, record_id):
-    fake_requests_post_response = fake_requests_post_response(201)
-    fake_requests_post_response.id = record_id
+def test_resource_id_returns_integer_on_post_request(mocker, record_id):
+    stub_request = mocker.MagicMock(
+        spec=requests.Request, method='POST')
+    stub_post_response = mocker.MagicMock(
+        spec=requests.Response, request=stub_request,
+        json=lambda: {'domain_record': {'id': record_id}})
 
-    response_result = Response(fake_requests_post_response)
+    mock_response = Response(stub_post_response)
 
-    assert response_result.resource_id == record_id
+    assert mock_response.resource_id == record_id
 
 
 @pytest.mark.parametrize('record_id', [1245542])
-def test_resource_id_returns_nothing_on_delete_request(
-        fake_requests_delete_response, record_id):
-    fake_requests_delete_response = fake_requests_delete_response(
-        204, record_id)
+def test_resource_id_returns_nothing_on_delete_request(mocker, record_id):
+    stub_request = mocker.MagicMock(
+        spec=requests.Request, method='DELETE')
+    stub_delete_response = mocker.MagicMock(
+        spec=requests.Response, request=stub_request,
+        json=lambda: {'domain_record': {'id': record_id}})
 
-    response_result = Response(fake_requests_delete_response)
+    mock_response = Response(stub_delete_response)
 
-    assert response_result.resource_id is None
+    assert mock_response.resource_id is None
 
 
-def test_raises_exception_on_bad_status_code(
-        fake_requests_post_response):
+def test_raises_exception_on_bad_status_code(mocker):
+    stub_request = mocker.MagicMock(
+        spec=requests.Request, method='POST')
+    stub_post_response = mocker.MagicMock(
+        spec=requests.Response, request=stub_request,
+        status_code=404, ok=False, url=None)
+
     with pytest.raises(RecordCreationFailure):
-        Response(fake_requests_post_response(404))
+        Response(stub_post_response)
 
 
-def test_prints_response_status_code_with_raised_exception(
-        fake_requests_post_response):
+def test_prints_response_status_code_with_raised_exception(mocker):
+    stub_request = mocker.MagicMock(
+        spec=requests.Request, method='POST')
+    stub_requests_response = mocker.MagicMock(
+        spec=requests.Response, request=stub_request,
+        status_code=500, ok=False, url=None)
+
     with pytest.raises(RecordCreationFailure) as exception:
-        Response(fake_requests_post_response(500))
+        Response(stub_requests_response)
 
     assert str(exception).find('500') > 0
 
 
-def test_prints_record_uri_with_raised_exception(
-        do_domain, fake_requests_post_response):
-    with pytest.raises(RecordCreationFailure) as exception:
-        Response(fake_requests_post_response(600))
+def test_prints_record_uri_with_raised_exception(mocker):
+    expected_uri = (
+        'https://api.digitalocean.com/v2/domains/grrbrr.ca/records')
 
-    expected_uri = ('https://api.digitalocean.com/v2/domains/'
-                    '%s/records' % do_domain)
+    stub_request = mocker.MagicMock(
+        spec=requests.Request, method='POST')
+    stub_requests_response = mocker.MagicMock(
+        spec=requests.Response, request=stub_request,
+        status_code=600, ok=False, url=expected_uri)
+
+    with pytest.raises(RecordCreationFailure) as exception:
+        Response(stub_requests_response)
 
     assert str(exception).find(expected_uri) > 0
