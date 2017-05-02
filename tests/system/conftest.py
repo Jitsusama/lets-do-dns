dc85d3d7ad5020314de60b5daedd0c900c8ab049
Joel Gerber <joel@grrbrr.ca>
2017-05-02 17:44:25 -0400

    Read system test configuration data from config.ini
    As the system tests require information that will be unique for each
    tester's environment, and are also intrinsically private
    information, I've moved all of this configuration into a config.ini
    file to be stored in the test/system directory.

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -1,3 +1,8 @@
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
 import os
 import pytest
 from requests import post
@@ -12,9 +17,24 @@ def os_environ_reset():
     os.environ.update(original_env)
 
 
-@pytest.fixture
-def do_record_id(create_response):
-    return create_response.json()['domain_record']['id']
+@pytest.fixture(scope='module')
+def test_configuration():
+    """Read test configuration from :file:`config.ini` file.
+    
+    The INI file must have a ``[DEFAULT]`` section containing the following
+    parameters:
+    
+    *  ``do_api_key``
+    *  ``do_domain``
+    *  ``do_hostname``"""
+    file_path = os.path.realpath(__file__)
+    directory_path = os.path.dirname(file_path)
+    config_file = '%s/config.ini' % directory_path
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    return config
 
 
 @pytest.fixture
@@ -28,9 +48,9 @@ def create_response(
               'data': request.function.__name__})
 
 
-@pytest.fixture
-def do_base_uri():
-    return 'https://api.digitalocean.com/v2/domains'
+@pytest.fixture()
+def do_api_key(test_configuration):
+    return test_configuration.get('DEFAULT', 'do_api_key')
 
 
 @pytest.fixture
@@ -38,22 +58,21 @@ def do_auth_header(do_api_key):
     return {'Authorization': 'Bearer %s' % do_api_key}
 
 
-@pytest.fixture()
-def do_api_key():
-    file_path = os.path.realpath(__file__)
-    directory_path = os.path.dirname(file_path)
-    api_key_file = '%s/digital_ocean_api_key.txt' % directory_path
-
-    api_key_file = open(api_key_file, 'r')
-
-    return api_key_file.readline()
+@pytest.fixture
+def do_base_uri():
+    return 'https://api.digitalocean.com/v2/domains'
 
 
 @pytest.fixture
-def do_domain():
-    return 'grrbrr.ca'
+def do_domain(test_configuration):
+    return test_configuration.get('DEFAULT', 'do_domain')
 
 
 @pytest.fixture
-def do_hostname():
-    return 'test-ssl-host'
+def do_hostname(test_configuration):
+    return test_configuration.get('DEFAULT', 'do_hostname')
+
+
+@pytest.fixture
+def do_record_id(create_response):
+    return create_response.json()['domain_record']['id']
