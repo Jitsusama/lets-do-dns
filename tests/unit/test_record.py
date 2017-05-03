1c6f0142e9d6714aba31a3be986e687bad2bfddc
Joel Gerber <joel@grrbrr.ca>
2017-05-03 18:32:34 -0400

    Cleanup variable names and stub_int stub
    The stub_int variable was introduced and fed into a autospec'd
    patch() to make the test_create_stores_record_id_internally test
    method to prepare for some changes that are about to happen in the
    Record() class.
    
    I also changed some variable names from having a stub prefix to
    having a mock prefix.

--- tests/unit/test_record.py
+++ tests/unit/test_record.py
@@ -2,15 +2,17 @@ from mock import call
 import pytest
 
 from lets_do_dns.acme_dns_auth.record import Record
+from lets_do_dns.do_domain.resource import Resource
 
 
 @pytest.mark.parametrize('input_record_id', [491834, 882342])
 def test_create_stores_record_id_internally(mocker, input_record_id):
-    mocker.patch(
-        'lets_do_dns.acme_dns_auth.record.Resource.create')
-    mocker.patch(
+    stub_int = mocker.patch(
         'lets_do_dns.acme_dns_auth.record.Resource.__int__',
         return_value=input_record_id)
+    mocker.patch(
+        'lets_do_dns.acme_dns_auth.record.Resource',
+        spec=Resource, __int__=stub_int)
 
     record = Record(None, None, None)
     record.create(None)
@@ -19,31 +21,31 @@ def test_create_stores_record_id_internally(mocker, input_record_id):
 
 
 def test_create_properly_calls_http_create(mocker):
-    stub_http_create = mocker.patch(
+    mock_resource = mocker.patch(
         'lets_do_dns.acme_dns_auth.record.Resource')
 
     record = Record(None, None, None)
     record.create('dummy-auth-token')
 
-    stub_http_create.assert_has_calls([
+    mock_resource.assert_has_calls([
         call(record, 'dummy-auth-token'),
         call().create()])
 
 
 def test_delete_properly_calls_http_delete(mocker):
-    stub_http_delete = mocker.patch(
+    mock_resource = mocker.patch(
         'lets_do_dns.acme_dns_auth.record.Resource')
 
     record = Record(None, None, None)
     record.delete()
 
-    stub_http_delete.assert_has_calls([
+    mock_resource.assert_has_calls([
         call(record),
         call().delete()])
 
 
 def test_printer_calls_printer(mocker):
-    stub_printer = mocker.patch(
+    mock_stdout = mocker.patch(
         'lets_do_dns.acme_dns_auth.record.stdout')
     record_id = 918342
 
@@ -51,4 +53,4 @@ def test_printer_calls_printer(mocker):
     record.id = record_id
     record.printer()
 
-    stub_printer.assert_called_once_with(record_id)
+    mock_stdout.assert_called_once_with(record_id)
